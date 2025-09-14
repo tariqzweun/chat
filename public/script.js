72c47c43664bbff6c7c08f7b12ff686b253fecfe
tariqzweun <zweuntariq@gmail.com>
2025-09-14 20:41:27 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,18 @@
-<script.js content from textdoc>
+const socket = io();
+
+const chat = document.getElementById("chat");
+const msg = document.getElementById("msg");
+
+function sendMsg() {
+  if (msg.value.trim() !== "") {
+    socket.emit("chat message", msg.value);
+    msg.value = "";
+  }
+}
+
+socket.on("chat message", (message) => {
+  const p = document.createElement("p");
+  p.textContent = message;
+  chat.appendChild(p);
+  chat.scrollTop = chat.scrollHeight; // ينزل لآخر رسالة
+});
