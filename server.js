f34ba828cdf21e65ebc8b3ade37dbb5d578ac5e4
tariqzweun <zweuntariq@gmail.com>
2025-09-14 20:43:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-<server.js content from textdoc>
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("🔵 مستخدم دخل");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔴 مستخدم خرج");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
