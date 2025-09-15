a8c25edc1bac02b65034f68bd267e4eab57c14e7
tariqzweun <zweuntariq@gmail.com>
2025-09-15 07:17:23 +0300

    Add files via upload

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6C5CE7',
+        accent: '#00BFA6'
+      }
+    },
+  },
+  plugins: [],
+}
